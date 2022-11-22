e221d03dbc126ba33be29c097dc5c3bf0384a8e3
Hardeep2401 <hardeep.singh@lpinfotech.com>
2022-11-22 15:08:22 +0530

    test

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      inter : ['inter,sans-serif'],
+    },
+    extend: { 
+    colors: {
+      'grayL': '#bcbcc2',
+      'blueD': '#111745',
+      'blueE': '#111740',
+      'blueF':'#3e7dff',
+      'blueG': '#080321',
+      'blueH': '#14102C',
+      'blueI': '#1f2756',
+      'blueJ': '#170E43',
+    },
+    width: {
+      '17' : '70px',
+      '18' : '74px',
+    },
+
+    height: {
+      '17' : '70px',
+      '18' : '74px'
+    }
+  },
+  },
+  plugins: [],
+}
