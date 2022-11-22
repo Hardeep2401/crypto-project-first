9ea5673c0545e896a6e47320c8b4d050ef9a1503
HarDeep <hardeep.singh@lpinfotech.com>
2022-11-22 22:45:12 +0530

    add color li

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,29 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      inter : ['inter,sans-serif'],
+      inter: ["inter,sans-serif"],
     },
-    extend: { 
-    colors: {
-      'grayL': '#bcbcc2',
-      'blueD': '#111745',
-      'blueE': '#111740',
-      'blueF':'#3e7dff',
-      'blueG': '#080321',
-      'blueH': '#14102C',
-      'blueI': '#1f2756',
-      'blueJ': '#170E43',
-    },
-    width: {
-      '17' : '70px',
-      '18' : '74px',
-    },
+    extend: {
+      colors: {
+        "gray-french": "#bcbcc2",
+        blueD: "#111745",
+        blueE: "#111740",
+        blueF: "#3e7dff",
+        blueG: "#080321",
+        blueH: "#14102C",
+        blueI: "#1f2756",
+        blueJ: "#170E43",
+      },
+      width: {
+        17: "70px",
+        18: "74px",
+      },
 
-    height: {
-      '17' : '70px',
-      '18' : '74px'
-    }
-  },
+      height: {
+        17: "70px",
+        18: "74px",
+      },
+    },
   },
   plugins: [],
-}
+};
